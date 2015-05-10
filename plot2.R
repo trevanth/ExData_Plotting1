37bc1e949bd036ea022e088f432c95d65985954d
Revanth Talari <t.revanth@gmail.com>
2015-05-10 18:41:07 -0400

    Commiting All The Plotted Graphs with background as white

--- plot2.R
+++ plot2.R
@@ -8,7 +8,7 @@ plot2 <- function()
         data$DateTime <- as.Date(data$DateTime, format = "%d/%m/%Y %H:%M:%S")
         req_data <- subset(data, DateTime == as.Date("2007-02-01") | DateTime == as.Date("2007-02-02"))
         req_data$DateTime <- strptime(paste(req_data$Date, req_data$Time), format = "%d/%m/%Y %H:%M:%S")
-        png(file = "plot2.png", width = 480, height = 480, units = "px")
+        png(file = "plot2.png", width = 480, height = 480, units = "px", bg = "white")
         plot(req_data$DateTime, req_data$Global_active_power, type = "l", ylab = "Global Active Power (kilowatts)", xlab = "")
         dev.off()
 }
